8cbea88db6dc36087531e367699825b523e33354
Michael Dudley <michael.dudley@improvingenterprises.com>
2016-11-20 00:19:46 -0600

    adding dependencies

--- config.js
+++ config.js
@@ -188,7 +188,7 @@ System.config({
       "base64-js": "npm:base64-js@0.0.8",
       "child_process": "github:jspm/nodelibs-child_process@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "ieee754": "npm:ieee754@1.1.6",
+      "ieee754": "npm:ieee754@1.1.8",
       "isarray": "npm:isarray@1.0.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
@@ -284,7 +284,20 @@ System.config({
       "brace-expansion": "npm:brace-expansion@1.1.6",
       "path": "github:jspm/nodelibs-path@0.1.0"
     },
+    "npm:miruken-callback@0.0.1": {
+      "miruken-core": "npm:miruken-core@0.0.1"
+    },
+    "npm:miruken-context@0.0.1": {
+      "miruken-callback": "npm:miruken-callback@0.0.1",
+      "miruken-core": "npm:miruken-core@0.0.1"
+    },
+    "npm:miruken-core@0.0.1": {
+      "reflect-metadata": "npm:reflect-metadata@0.1.8"
+    },
     "npm:miruken-validate@0.0.1": {
+      "miruken-callback": "npm:miruken-callback@0.0.1",
+      "miruken-context": "npm:miruken-context@0.0.1",
+      "miruken-core": "npm:miruken-core@0.0.1",
       "validate.js": "npm:validate.js@0.10.0"
     },
     "npm:mkdirp@0.5.1": {
